9aaa72cee31335fab449956a9c0ac5bafb1db4ea
Wes McC <wkmccowin@yahoo.com>
2019-07-24 21:27:05 -0700

    testing deployment

--- find-a-book/routes/index.js
+++ find-a-book/routes/index.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 
 
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 module.exports = router;
